3c33bec770f5e154ee2727dcb0132db3e5ef035e
f3nrisw0lf <rodrickcorpuz@gmail.com>
2022-11-01 01:55:33 +0800

    Added boilerplate for React Query, React Router. Added Jsconfig for src path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClientProvider } from 'react-query';
 
-function App() {
+import Forum from 'src/pages/Forum';
+import ClientProvider from 'src/helper/ClientProvider.js';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={ClientProvider}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Forum />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
-
-export default App;
---
+++ src/api/forumApi.js
@@ -0,0 +1,7 @@
+import AxiosInstance from 'src/helper/AxiosInstance';
+
+async function getPosts() {}
+async function getPost(postId) {}
+async function createPost() {}
+
+export { getPosts, getPost, createPost };
