a52bb9eaf46b8817180df4297e5cc8cbaaf5dc37
f3nrisw0lf <rodrickcorpuz@gmail.com>
2022-11-09 00:27:14 +0800

    Changed API URL format

--- src/helper/AxiosInstance.js
+++ src/helper/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const API_URL = process.env.API_URL || 'http://localhost:8080/';
+const { API_URL = 'http://localhost:8080/' } = process.env;
 
 const AxiosInstance = axios.create({
   baseURL: API_URL,
