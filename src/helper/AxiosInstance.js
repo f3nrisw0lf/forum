37ac8e85da3230cfa1826c070fb4ec351f4bfd40
f3nrisw0lf <rodrickcorpuz@gmail.com>
2022-11-09 00:41:27 +0800

    Removed Dotenv, Changed API URL TO REACT APP API URL

--- src/helper/AxiosInstance.js
+++ src/helper/AxiosInstance.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config();
 
-const { API_URL = 'http://localhost:8080/' } = process.env;
+const { REACT_APP_API_URL = 'http://localhost:8080/' } = process.env;
 
 const AxiosInstance = axios.create({
-  baseURL: API_URL,
+  baseURL: REACT_APP_API_URL,
 });
 
 export default AxiosInstance;
